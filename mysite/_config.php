2364794faccddc1e7108ebf9677596a2feb5a109
Marcz Hermo <marcz@lab1521.com>
2017-03-13 21:10:28 +1300

    Use _ss_environment.php

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,15 +3,10 @@
 global $project;
 $project = 'mysite';
 
-global $databaseConfig;
-$databaseConfig = array(
-	'type' => 'MySQLDatabase',
-	'server' => 'localhost',
-	'username' => 'root',
-	'password' => 'root',
-	'database' => 'ss3app',
-	'path' => ''
-);
+global $database;
+$database = 'ss3_app';
+
+require_once('conf/ConfigureFromEnv.php');
 
 // Set the site locale
 i18n::set_locale('en_US');
